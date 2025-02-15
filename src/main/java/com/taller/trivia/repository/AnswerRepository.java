af28eaa3884aa7b6fbb853afb22b519a71bc6e1a
tomas <tomascatalini@gmail.com>
2025-02-15 11:16:44 -0300

    Delete repositories

--- src/main/java/com/taller/trivia/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.taller.trivia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.taller.trivia.model.Answer;
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-
-}
--- src/main/java/com/taller/trivia/repository/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.taller.trivia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.taller.trivia.model.Game;
-
-public interface GameRepository extends JpaRepository<Game, Long> {
-
-}
