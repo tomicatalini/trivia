c7dbad6d6b66ed835fce7afe73868f6af2390345
tomas <tomascatalini@gmail.com>
2025-02-15 11:27:42 -0300

    Mod git ignore para messages properties. Objeto que captura los mensajes.

---
+++ src/main/java/com/taller/trivia/util/ErrorMassageLoader.java
@@ -0,0 +1,18 @@
+package com.taller.trivia.util;
+
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+public class ErrorMassageLoader {
+    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle("messages");
+
+    public static String getMessage(String code, Object... args) {
+            try {
+                String message = resourceBundle.getString(code);
+
+                return MessageFormat.format(message, args);
+            } catch (Exception e) {
+                return "Mensaje no encontrado";
+            }
+    }
+}
